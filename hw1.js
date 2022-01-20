fc5ef5632d1353d2740fb18fb1e131ae673cf967
rachel390 <70980217+rachel390@users.noreply.github.com>
2022-01-19 19:44:33 -0500

    Add files via upload

--- hw1.js
+++ hw1.js
@@ -1,61 +1,65 @@
+/*
+var colors = [color(0, 0, 0), color(255, 0, 0), color(0, 0, 255), color(0, 255, 0)];
+var randomColor = colors[floor(random(0, colors.length))];
+fill(randomColor);
+*/
 
-
+let r = 50;
+let g = 100;
+let b = 100;
 function setup() {
   createCanvas (800, 800); 
-  //noStroke();
-  noLoop();
+  noStroke();
+  //noLoop();
 }
 
 
 function draw() {
-  background (200, 200, 255); 
-  //ellipse(width/2, height/2, width/4,height/4);
-  sixCircle(width/2,width/2, width/4,6);
+  background (0); 
+  fill('magenta');
+  ellipse(width/2, height/2, width/4,height/4);
+  fill(r,g,b);
+  sixCircle(width/2,width/2, width/4, 5 ,4);
   //drawCircle(width/2,width/2, width/4, 6);
   
+  
 }
-function drawCircle(x,y,  diameter, level) {
 
-  //const tt = (126 * level) / 4.0;
-  //fill(tt);
-  fill('red');
-  let pointCount = 6;
-  //side six
-  ellipse(x, y, diameter, diameter);
-  //sixCircle(x, y, diameter);
-  if (level > 1) {
-    level = level - 1;
-    drawCircle(diameter * Math.cos(0) + x,diameter * Math.sin(0) + y, diameter/2, level);
-    //drawCircle(diameter * Math.cos(TWO_PI/6) + x, Math.sin(TWO_PI/6) + y, diameter/2, level);
-  }
- /*
-  for(let i =0; i < TWO_PI; i += TWO_PI / 6) {
-    let xex = diameter * Math.cos(i) + x;
-    let y = diameter * Math.sin(i) + x;
-    if (level > 1) {
-      level = level - 1;
-      //drawCircle(xex, y, diameter/2, level);
-    }
-    circle(xex,y,diameter/2);
-  */
-    //drawCircle(xex, y, diameter/2, level);
-    //drawCircle( diameter * Math.cos(i) - xex,  diameter * Math.sin(i)-y, diameter/2, level);
-  //}
-}
-function sixCircle(x,y,diameter,level) {
-   fill('blue');
-   ellipse(x, y, diameter, diameter);
+function sixCircle(x,y,diameter,level,k) {
+   switch(level) {
+     case 1:
+       fill('purple');
+       break;
+     case 2:
+       fill('blue');
+       break;
+     case 3:
+       fill('green');
+       break;
+     case 4:
+       fill('yellow');
+       break;
+     case 5:
+       fill('orange');
+       break;
+     case 6:
+       fill('red');
+       break;
+   }
+   
+   //circle(x, y, diameter);
    for(let i =0; i < TWO_PI; i += TWO_PI / 6) {
-    let xex = diameter * Math.cos(i) + x;
-    let myy = diameter * Math.sin(i) + y;
-    circle(xex,myy,diameter/2);
+    let xex = diameter * Math.cos(i + mouseX/width)+ x;
+    let myy = diameter * Math.sin(i + mouseX/width) +y;
+    //circle(xex,myy,diameter- (mouseY/4));
+    circle(xex, myy, (diameter)*(1-mouseY/height));
    }
    if (level > 1) {
      level = level - 1;
      for(let i =0; i < TWO_PI; i += TWO_PI / 6) {
       let xex = diameter * Math.cos(i) + x;
       let myy = diameter * Math.sin(i) + y;
-      sixCircle(xex, myy, diameter/2, level);
+      sixCircle(xex, myy, diameter/2, level, k * 2);
      }
    }
 }
